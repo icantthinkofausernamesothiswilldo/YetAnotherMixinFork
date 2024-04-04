24ba7e9344e7a82562f110fe21fab66189c5237d
icantthinkofausernamesothiswilldo <161003294+icantthinkofausernamesothiswilldo@users.noreply.github.com>
2024-04-04 22:07:27 +0300

    Create TransformationTool.java

---
+++ src/main/java/org/spongepowered/asm/mixin/TransformationTool.java
@@ -0,0 +1,8 @@
+package org.spongepowered.asm.mixin;
+
+import java.lang.instrument.Instrumentation;
+
+public class TransformationTool {
+  public static void premain(String agentArgs, Instrumentation instrumentation) {
+  }
+}
